c5dfd9c7cb775573a3162587623b94fb34e0f74d
Heinrich <mt.quyenx@gmail.com>
2025-01-09 21:25:40 +0700

    Add npm and update elements structure in HomePage

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -1,37 +1,27 @@
 import { expect, Locator, Page } from "@playwright/test";
 
 export class HomePage {
-    // Define selectors
+    // Define page object
     readonly page: Page
-    readonly imgLogo: Locator
-    readonly menuTuyenSinh: Locator
-    readonly menuChuongTrinh: Locator
-    readonly menuHoTroSV: Locator
-    readonly menuTinTuc: Locator
-    readonly btnHoiDap: Locator
-    readonly sliderHero: Locator
-    readonly infoCard1Title: Locator
-    readonly infoCard1Desc: Locator
-    readonly infoCard2Title: Locator
-    readonly infoCard2Desc: Locator
-    readonly infoCard3Title: Locator
-    readonly infoCard3Desc: Locator
-    // Init selectors using constructor
+    // Init constructor
     constructor(page: Page) {
         this.page = page
-        this.imgLogo = page.locator('img.logo-img.site-logo-img')
-        this.menuTuyenSinh = page.locator("li[id='menu-item-5291'] a[class=' main-menu-link main-menu-link-top']")
-        this.menuChuongTrinh = page.locator("li[id='menu-item-5863'] a[class=' main-menu-link main-menu-link-top']")
-        this.menuHoTroSV = page.locator("li[id='menu-item-7947'] a[class=' main-menu-link main-menu-link-top']")
-        this.menuTinTuc = page.locator("li[id='menu-item-6407'] a[class=' main-menu-link main-menu-link-top']")
-        this.btnHoiDap = page.locator('#ctabutton')
-        this.sliderHero = page.locator('#rev_slider_53_1')
-        this.infoCard1Title = page.locator('(//div[@id="eluid8a60b29e"]/descendant::div[@class="znColumnElement-innerContent"])[2]/descendant::div')
-        this.infoCard1Desc = page.locator('//div[@id="eluid8a60b29e"]//span[1]')
-        this.infoCard2Title = page.locator('(//div[@id="eluid69322cbf"]/descendant::div[@class="znColumnElement-innerContent"])[2]/descendant::div')
-        this.infoCard2Desc = page.locator('//div[@id="eluid69322cbf"]//span[1]')
-        this.infoCard3Title = page.locator('(//div[@id="eluidc68c0e7a"]/descendant::div[@class="znColumnElement-innerContent"])[2]/descendant::div')
-        this.infoCard3Desc = page.locator('//div[@id="eluidc68c0e7a"]//span[1]')
+    }
+    // Define elements
+    elements = {
+        imgLogo: () => this.page.locator('img.logo-img.site-logo-img'),
+        menuTuyenSinh: () => this.page.locator("li[id='menu-item-5291'] a[class=' main-menu-link main-menu-link-top']"),
+        menuChuongTrinh: () => this.page.locator("li[id='menu-item-5863'] a[class=' main-menu-link main-menu-link-top']"),
+        menuHoTroSV: () => this.page.locator("li[id='menu-item-7947'] a[class=' main-menu-link main-menu-link-top']"),
+        menuTinTuc: () => this.page.locator("li[id='menu-item-6407'] a[class=' main-menu-link main-menu-link-top']"),
+        btnHoiDap: () => this.page.locator('#ctabutton'),
+        sliderHero: () => this.page.locator('#rev_slider_53_1'),
+        infoCard1Title: () => this.page.locator('(//div[@id="eluid8a60b29e"]/descendant::div[@class="znColumnElement-innerContent"])[2]/descendant::div'),
+        infoCard1Desc: () => this.page.locator('//div[@id="eluid8a60b29e"]//span[1]'),
+        infoCard2Title: () => this.page.locator('(//div[@id="eluid69322cbf"]/descendant::div[@class="znColumnElement-innerContent"])[2]/descendant::div'),
+        infoCard2Desc: () => this.page.locator('//div[@id="eluid69322cbf"]//span[1]'),
+        infoCard3Title: () => this.page.locator('(//div[@id="eluidc68c0e7a"]/descendant::div[@class="znColumnElement-innerContent"])[2]/descendant::div'),
+        infoCard3Desc: () => this.page.locator('//div[@id="eluidc68c0e7a"]//span[1]')
     }
     // Define page methods
     async visit() {
@@ -41,24 +31,24 @@ export class HomePage {
         await expect(this.page).toHaveTitle(title) 
     }
     async assertLogoVisible() {
-        await expect(this.imgLogo).toBeVisible()
+        await expect(this.elements.imgLogo()).toBeVisible()
     }
     async assertTextMenu(
         textTuyenSinh: string,
         textChuongTrinh: string,
         textHoTroSV: string,
         textTinTuc: string) {
-        await expect(this.menuTuyenSinh).toContainText(textTuyenSinh)
-        await expect(this.menuChuongTrinh).toContainText(textChuongTrinh)
-        await expect(this.menuHoTroSV).toContainText(textHoTroSV)
-        await expect(this.menuTinTuc).toContainText(textTinTuc)
+        await expect(this.elements.menuTuyenSinh()).toContainText(textTuyenSinh)
+        await expect(this.elements.menuChuongTrinh()).toContainText(textChuongTrinh)
+        await expect(this.elements.menuHoTroSV()).toContainText(textHoTroSV)
+        await expect(this.elements.menuTinTuc()).toContainText(textTinTuc)
     }
     async assertButtonFAQ(textFAQ: string) {
-        await expect(this.btnHoiDap).toBeVisible()
-        await expect(this.btnHoiDap).toContainText(textFAQ)
+        await expect(this.elements.btnHoiDap()).toBeVisible()
+        await expect(this.elements.btnHoiDap()).toContainText(textFAQ)
     }
     async assertHeroSlider() {
-        await expect(this.sliderHero).toBeVisible()
+        await expect(this.elements.sliderHero()).toBeVisible()
     }
     async assertInfoCards1(
         textInfoCard1TitleNum: string,
@@ -66,9 +56,9 @@ export class HomePage {
         textInfoCard1Desc1: string,
         textInfoCard1Desc2: string,
     ) {
-        await expect(this.infoCard1Title).toContainText(textInfoCard1TitleNum)
-        await expect(this.infoCard1Title).toHaveAttribute('data-text-after', textInfoCard1TitleSuffix)
-        await expect(this.infoCard1Desc).toContainText(textInfoCard1Desc1 + textInfoCard1Desc2)
+        await expect(this.elements.infoCard1Title()).toContainText(textInfoCard1TitleNum)
+        await expect(this.elements.infoCard1Title()).toHaveAttribute('data-text-after', textInfoCard1TitleSuffix)
+        await expect(this.elements.infoCard1Desc()).toContainText(textInfoCard1Desc1 + textInfoCard1Desc2)
     }
     async assertInfoCards2(
         textInfoCard2TitleNum: string,
@@ -76,15 +66,15 @@ export class HomePage {
         textInfoCard2Desc1: string,
         textInfoCard2Desc2: string,
     ) {
-        await expect(this.infoCard2Title).toContainText(textInfoCard2TitleNum)
-        await expect(this.infoCard2Title).toHaveAttribute('data-text-after', textInfoCard2TitleSuffix)
-        await expect(this.infoCard2Desc).toContainText(textInfoCard2Desc1 + textInfoCard2Desc2)
+        await expect(this.elements.infoCard2Title()).toContainText(textInfoCard2TitleNum)
+        await expect(this.elements.infoCard2Title()).toHaveAttribute('data-text-after', textInfoCard2TitleSuffix)
+        await expect(this.elements.infoCard2Desc()).toContainText(textInfoCard2Desc1 + textInfoCard2Desc2)
     }
     async assertInfoCards3(
         textInfoCard3Title: string,
         textInfoCard3Desc: string
     ) {
-        await expect(this.infoCard3Title).toContainText(textInfoCard3Title)
-        await expect(this.infoCard3Desc).toContainText(textInfoCard3Desc)
+        await expect(this.elements.infoCard3Title()).toContainText(textInfoCard3Title)
+        await expect(this.elements.infoCard3Desc()).toContainText(textInfoCard3Desc)
     }
 }
